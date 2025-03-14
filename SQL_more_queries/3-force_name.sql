57bbe5346dcbb7416a28da82803be9e63bd4802d
IH-honette <h.ihozo@alustudent.com>
2025-03-14 14:38:57 -0700

    MORE SQL

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+-- Create the table force_name if it does not already exist
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,                   -- Column id of type INT
+    name VARCHAR(256) NOT NULL -- Column name of type VARCHAR(256) that cannot be NULL
+);
+
